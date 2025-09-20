c21314c938c04ea513c5f39cd5de5b414c501ca2
Karan <karanjamwal2004@gmail.com>
2025-09-20 12:51:01 +0530

    header created

--- assembly-endgame/src/components/Header.jsx
+++ assembly-endgame/src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <h1>Assembly: Endgame</h1>
+      <p>
+        Guess the word within 8 attempts to keep the programming world safe from
+        Assembly!
+      </p>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
